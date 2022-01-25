f105463221927a7291b9af2f16d18ddf9b16e10c
felipe basilio amaral <felipeambaas@gmail.com>
2022-01-25 10:32:32 -0300

    inicio de projeto

---
+++ script.js
@@ -0,0 +1,90 @@
+function acao() {
+    var modal = document.querySelector(".modal")
+
+    modal.style.display = 'block'
+}
+
+function fechar() {
+    var modal = document.querySelector(".modal")
+    
+    modal.style.display = 'none'
+}
+
+function entre() {
+    var mold = document.querySelector(".mold")
+
+    mold.style.display = 'block'
+}
+
+function sumir() {
+    var mold = document.querySelector(".mold")
+
+    mold.style.display = 'none'
+}
+
+function fund() {
+    var mod = document.querySelector(".mod")
+
+    mod.style.display = 'block'
+}
+
+function sub() {
+    var mod = document.querySelector(".mod")
+
+    mod.style.display = 'none'
+}
+
+function conf() {
+    var modall = document.querySelector(".modall")
+
+    modall.style.display = 'block'
+}
+function fec() {
+    var modall = document.querySelector(".modall")
+
+    modall.style.display = 'none'
+}
+
+function abr() {
+    var cai = document.querySelector(".cai")
+
+    cai.style.display = 'block'
+}
+function fech() {
+    var cai = document.querySelector(".cai")
+
+    cai.style.display = 'none'
+}
+
+function apa() {
+    var cax = document.querySelector(".cax")
+
+    cax.style.display = 'block'
+}
+function feh() {
+    var cax = document.querySelector(".cax")
+
+    cax.style.display = 'none'
+}
+
+function apar() {
+    var caxh = document.querySelector(".caxh")
+
+    caxh.style.display = 'block'
+}
+function fehc() {
+    var caxh = document.querySelector(".caxh")
+
+    caxh.style.display = 'none'
+}
+
+function pon() {
+    var opc = document.querySelector(".opc")
+
+    opc.style.display = 'block'
+}
+function enc() {
+    var opc = document.querySelector(".opc")
+
+    opc.style.display = 'none'
+}
